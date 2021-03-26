f2211def2e19083fab8a7506cbcd97ecc3f48d5f
Canu Fabien <fabien.canu@gmail.com>
2021-03-26 10:26:07 +0100

    UP

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -5,14 +5,17 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Page;
+use App\Entity\Portfolio;
+use Symfony\Component\Validator\Constraints as Assert;
 
-class PagesController extends AbstractController
-{
+
+class PagesController extends AbstractController {
+ 
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
-    {
+    public function index(): Response {
         return $this->render('pages/index.html.twig', [
             'title' => 'HOME',
         ]);
@@ -21,10 +24,59 @@ class PagesController extends AbstractController
     /**
      * @Route("/contact", name="contact")
      */
-    public function contact(): Response
-    {
+    public function contact(): Response {
         return $this->render('pages/contact.html.twig', [
             'title' => 'CONTACT',
+        ]);
+    }
+
+    /**
+     * @Route("/portfolio/{slug}", name="porfolio")
+     */
+    public function portfolio(string $slug): Response {
+        $repository = $this->getDoctrine()->getRepository(Portfolio::class);
+        //$Portfolio = $repository->find($id);
+        $Portfolios = $repository->findBy(array("user" => $slug));
+
+        return $this->render('pages/portfolio.html.twig', [
+            'portfolios' => $Portfolios
+        ]);
+    }   
+
+    /**
+     * @Route("/page/{id}", name="page")
+     */
+    public function page(int $id): Response {
+        $repository = $this->getDoctrine()->getRepository(Page::class);
+        $Page = $repository->find($id);
+
+        return $this->render('pages/page.html.twig', [
+            'title' => $Page->getTitle(),
+            'content' => $Page->getContent(),
+        ]);
+    }
+
+    /**
+     * @Route("/createpage", name="createpage")
+     */
+    public function createpage(): Response {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $Page = new Page();
+        $Page->setTitle('Nouveau titre'.rand());
+        $Page->setContent('lorem ipsum ...');
+        $Page->setCreatedAt(new \DateTime());
+
+        $entityManager->persist($Page);
+        $entityManager->flush();
+
+        // Liste pages
+        $repository = $this->getDoctrine()->getRepository(Page::class);
+        $pages = $repository->findAll();
+
+        return $this->render('pages/createpage.html.twig', [
+            'info' => 'Nouvelle page insérée '.$Page->getId(),
+            'pages' => $pages
         ]);
     }
 }
---
+++ src/Entity/Portfolio.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PortfolioRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PortfolioRepository::class)
+ */
+class Portfolio
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): ?string
+    {
+        return $this->user;
+    }
+
+    public function setUser(string $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+}
